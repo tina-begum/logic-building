b88461ad1f2173c837b85f710f46980cc1873419
tina-begum <quantcom01@gmail.com>
2026-09-08 19:34:25 +0530

    Basic logic building problems

--- Fundamentals/Problem22.java
+++ Fundamentals/Problem22.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Problem22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int originalNumber = n;
+        int num = sc.nextInt();
+        int originalNumber = num;
         int sum = 0;
-        while (n > 0) {
-            sum +=  (int)(Math.pow((n % 10), 3));
-            n /= 10;
+        while (num > 0) {
+            sum +=  (int)(Math.pow((num % 10), 3));
+            num /= 10;
         }
         if (originalNumber == sum) {
             System.out.println(originalNumber + " is Armstrong number.");
